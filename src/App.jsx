00a56ce4fb42e364940aec3257259334684b546e
Tianya Chen <tianyakc@gmail.com>
2025-04-24 21:58:54 -0400

    initialized the react root elements main.jsx and App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styles from "./App.module.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+
+const App = () => {
+  return (
+    <div className={styles.App}>
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+    </div>
+  );
+};
+
+export default App;
