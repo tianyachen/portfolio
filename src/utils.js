b781cf5eb1a4cce37a23e394c99ad3e0034e6ee4
Tianya Chen <tianyakc@gmail.com>
2025-04-25 13:12:22 -0400

    resovled the lastest vite static asset handling issue where vite can't find the assest folder during build and dev

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,17 @@
-export const getImageUrl = (path) => {
-  const relPath = `/assets/${path}`;
-  return new URL(relPath, import.meta.url).href;
+const imageModules = import.meta.glob("./assets/**/*.{png,jpg,jpeg,svg}", {
+  eager: true,
+  query: "?url",
+  import: "default",
+});
+
+export const getImageUrl = (relativePath) => {
+  return imageModules[`./assets/${relativePath}`];
 };
+
+// export const allImagePaths = Object.keys(imageModules).map((path) =>
+//   path.replace("./assets/", "")
+// );
+
+// export const getImageUrl = (path) => {
+//   return new URL(`./assets/${path}`, import.meta.url).href;
+// };
